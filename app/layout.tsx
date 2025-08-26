62018375d014fbb61d90e755ff9b60b65c5f151a
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2025-08-26 09:55:53 +0530

    google search engine verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,6 +19,9 @@ export const metadata: Metadata = {
   title: "Ayush Bahuguna",
   description:
     "Hi there! I'm Ayush (he/him), a full-stack developer based out of India. I'm self-taught, always keen to make dynamic & functional websites! Drop a hello & let's build a fantastic website together! ",
+  verification: {
+    google: "r0Pvd8yd6-X9oVjOgtMP66aZjtU0zg9LV_4mf_0Oyn4",
+  },
 };
 
 export default function RootLayout({
